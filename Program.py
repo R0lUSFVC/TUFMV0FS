ab0829d2285bfbb1997dfccb52d06f22b0de98a8
HotPogger <will.anstey2005@gmail.com>
2020-11-11 11:47:44 -0500

    f

--- Program.py
+++ Program.py
@@ -15,4 +15,6 @@ Game.main(platform) ##Get platform-dependant game files
 
 response = requests.get("https://raw.github.com/R0lUSFVC/TUFMV0FS/master/GameCode.py")
 with open("Game.py", "w") as file:
-    file.write(response.text) ##Get
+    file.write(response.text) ##Get game code
+
+Game.runCode()
